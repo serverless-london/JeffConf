5e08ad9ff11ab28c1f36b8a7d86f9792f019b94f
Ant Stanley <antstanley@users.noreply.github.com>
2017-06-06 15:34:40 +0100

    Add basic routing to site (#2)
    * Add onload event to check for paths
    
    * add pushstate for url changes
    
    * I should really use a linter
    
    * yup ... Linter
    
    * again
    
    * hopefully this works
    
    * unbreaking the JS
    
    * Add some debugging code
    
    * this should work. remove leading slash in url path
    
    * I'm an idiot
    
    * Add sitemap to validation
    
    * learn some syntax for a change
    
    * more to learn still
    
    * remove sitemap validation

--- src/jeff.js
+++ src/jeff.js
@@ -2,10 +2,45 @@ function changeArticle(elementID) {
             var contentBody = document.getElementById("contentBody").children;
             for (var i = 0; i < contentBody.length; i++) {
                 if (contentBody[i].id == elementID) {
-                    contentBody[i].className = "dtc w-100"
+                    contentBody[i].className = "dtc w-100";
+                    history.pushState(null,null,elementID)
+//                    console.log(elementID)
                 } else {
                 contentBody[i].className ="dn";
                 }
-            }
-    
-} 
+            }   
+}
+
+function loadPage(){
+
+
+	var siteMap = {
+		"home": true,
+		"agenda": true,
+		"speakers": true,
+		"venue": true,
+		"partners": false,
+		"coc": true,
+		"tickets": true
+	};
+
+//	console.log (window.location.pathname)
+	var sitePath=window.location.pathname.replace("/","");
+
+	if(sitePath.length>0){
+		changeArticle(sitePath);
+/*		
+		if (typeof siteMap(sitePath) === 'object') {
+			if (siteMap(sitePath)) {
+				changeArticle(sitePath)
+			} else {
+				changeArticle('home')
+			}
+		} else {
+			changeArticle('home')
+		}
+*/
+	} else {
+		changeArticle('home')
+	};
+}
