755837324eefcd4f22b17e2865a2eaff40d1d087
Ant Stanley <antstanley@users.noreply.github.com>
2017-06-17 11:48:13 +0100

    change routing to work on popstate event

--- src/jeff.js
+++ src/jeff.js
@@ -1,38 +1,42 @@
+
+function changePage (elementID) {
+	history.pushState(null,null,elementID)
+	console.log(elementID)
+}
+
 function changeArticle(elementID) {
             var contentBody = document.getElementById("contentBody").children;
             for (var i = 0; i < contentBody.length; i++) {
                 if (contentBody[i].id == elementID) {
                     contentBody[i].className = "dtc w-100";
-                    history.pushState(null,null,elementID)
-//                    console.log(elementID)
+                    console.log(elementID)
                 } else {
                 contentBody[i].className ="dn";
                 }
             }   
 }
 
-function loadPage(){
-
+function loadPage(event){
 
 	var siteMap = {
 		"home": true,
 		"agenda": true,
 		"speakers": true,
 		"venue": true,
-		"partners": false,
+		"partners": true,
 		"coc": true,
 		"tickets": true
 	};
-	
-//	console.log (window.location.pathname)
+
+	console.log (window.location.pathname)
 	if (window.location.pathname != "") {
 		
 		var sitePath=window.location.pathname.replace("/","");
-
+		console.log(sitePath);
 		if(sitePath.length>0){
 			changeArticle(sitePath);
-	/*		
-			if (typeof siteMap(sitePath) === 'object') {
+			
+			if (siteMap(sitePath)) {
 				if (siteMap(sitePath)) {
 					changeArticle(sitePath)
 				} else {
@@ -41,10 +45,12 @@ function loadPage(){
 			} else {
 				changeArticle('home')
 			}
-	*/
+	
 		} else {
 			changeArticle('home')
 		};
 
 	};
 }
+
+window.onpopstate = loadPage (event);
