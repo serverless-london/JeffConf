9eca81c14cd0eaffa43c968aadabb1ded1787792
Ant Stanley <antstanley@users.noreply.github.com>
2017-06-17 12:32:26 +0100

    I'm an idiot

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('minify', function() {
 });
 
 gulp.task('copy', function(){
-  return gulp.src(['src/_redirects','src/*.xml','src/*.css','*.js'])
+  return gulp.src(['src/_redirects','src/*.xml','src/*.css','src/*.js'])
     .pipe(gulp.dest('dist'));
 });
 
